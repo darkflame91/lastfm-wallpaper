f28fec48727d0e0e73db42d2b71c150bbe25af2f
darkflame91 <darkflame91@gmail.com>
2017-02-13 18:33:19 +0530

    Takes last played from lastfm and displays the image

--- lastfm-wallpaper.py
+++ lastfm-wallpaper.py
@@ -1,6 +1,15 @@
 from PIL import Image, ImageFilter, ImageEnhance, ImageFont, ImageDraw
+import requests
+import json
+import time
+import StringIO
+import urllib
 
+USERNAME = 'darkflame91'
+
+timer = 10
 size = 200
+
 def getFont(txt):
 	fontsize = 1
 	fontname = "Roboto.ttf"
@@ -12,22 +21,52 @@ def getFont(txt):
 	font = ImageFont.truetype(fontname, fontsize)
 	return font
 
-album = Image.open("Master_Puppets.jpg")
-black = Image.new('RGB',(1920,1080),(0,0,0))
-print album
+cover = Image.new('RGB',(size,size),(0,0,0))
+cover = Image.open("Master_Puppets.jpg")
 
-album.thumbnail((size,size))
-#album = ImageEnhance.Sharpness(album).enhance(2.0)
-#album = album.filter(ImageFilter.SMOOTH_MORE)
-#album = album.filter(ImageFilter.SHARPEN)
-# album = album.filter(ImageFilter.SMOOTH_MORE)
-# album = album.filter(ImageFilter.SHARPEN)
-# album = album.filter(ImageFilter.SMOOTH_MORE)
-# album = album.filter(ImageFilter.SHARPEN)
-# album = album.filter(ImageFilter.SMOOTH_MORE)
-# album = album.filter(ImageFilter.SHARPEN)
-#album = album.filter(ImageFilter.FIND_EDGES)
-album = album.convert('LA')
+
+
+
+pres = ""
+artist = "Unknown Artist"
+album = "Unknown Album"
+track = "Unknown Track"
+cover = Image.open("default.jpg")
+try:
+	res = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user='+USERNAME+'&api_key=1d9f9f8b8e813cd77cc15f1978ca6c0f&format=json&limit=1')
+	pres = json.loads(res.text)
+except Exception as err:
+	print err
+	time.sleep(timer)
+
+if pres['recenttracks']['track'][0]['artist']['#text'] != '':
+	artist = pres['recenttracks']['track'][0]['artist']['#text']
+if pres['recenttracks']['track'][0]['album']['#text'] != '':
+	album = pres['recenttracks']['track'][0]['album']['#text']
+if pres['recenttracks']['track'][0]['name'] != '':
+	track = pres['recenttracks']['track'][0]['name']
+
+if pres['recenttracks']['track'][0]['image'][-1]['#text'] != '':
+	cover = Image.open(requests.get(pres['recenttracks']['track'][0]['image'][-1]['#text'], stream=True).raw)
+
+
+
+
+
+black = Image.new('RGB',(1920,1080),(0,0,0))
+
+cover.thumbnail((size,size))
+#cover = ImageEnhance.Sharpness(cover).enhance(2.0)
+#cover = cover.filter(ImageFilter.SMOOTH_MORE)
+#cover = cover.filter(ImageFilter.SHARPEN)
+# cover = cover.filter(ImageFilter.SMOOTH_MORE)
+# cover = cover.filter(ImageFilter.SHARPEN)
+# cover = cover.filter(ImageFilter.SMOOTH_MORE)
+# cover = cover.filter(ImageFilter.SHARPEN)
+# cover = cover.filter(ImageFilter.SMOOTH_MORE)
+# cover = cover.filter(ImageFilter.SHARPEN)
+#cover = cover.filter(ImageFilter.FIND_EDGES)
+cover = cover.convert('LA')
 # for i in range(300):
 # 	for j in range(300):
 # 		temp = px[i,j]
@@ -47,11 +86,11 @@ album = album.convert('LA')
 # 			px[i,j] = 0
 
 
-black.paste(album,(1700,810))
+black.paste(cover,(1700,810))
 draw = ImageDraw.Draw(black)
 lastsize = 1010
 
-for txt in ["Track Name","Artist Name","Album Name"]:
+for txt in [track,artist,album]:
 	font = getFont(txt)
 	draw.text((1700, lastsize+8), txt, font=font)
 	lastsize += font.getsize(txt)[1]
